235bb37b93347cad6b69a6c516f0fc1c7b22dfe8
William Hemmingsson <william.hemmingsson@storytel.com>
2023-04-24 10:00:10 +0200

    Fixed rendering and hover of outputs

--- component.js
+++ component.js
@@ -1,92 +1,88 @@
-
 class Component extends ComponentBase {
-    constructor(x, y, w, h) {
-        super(true, true, true, true);
-        this.x = x;
-        this.y = y;
-        this.color = new Color(50, 360, 100);
-        this.w = w ? w : 100;
-        this.h = h ? h : 100;
+  constructor(x, y, w, h) {
+    super(true, true, true, true);
+    this.x = x;
+    this.y = y;
+    this.color = new Color(50, 360, 100);
+    this.w = w ? w : 100;
+    this.h = h ? h : 100;
 
-        // Inputs and outputs are connectors
-        this.inputs = [];
-        this.outputs = [];
+    // Inputs and outputs are connectors
+    this.inputs = [];
+    this.outputs = [];
+  }
+
+  _applyBorder() {
+    if (this.mouseIsOver(false)) {
+      strokeWeight(2);
+      stroke(100, 0, 100);
+    } else {
+      noStroke();
     }
+  }
 
-    _applyBorder() {
-        if (this.mouseIsOver(false)) {
-            strokeWeight(2);
-            stroke(100, 0, 100);
-        }
-        else {
-            noStroke();
-        }
-    }
+  _positionAndScaleInputs() {
+    const availableConnectorHeight = Math.min(this.h / this.inputs.length, 75);
 
-    _positionAndScaleInputs() {
-        const availableConnectorHeight = Math.min(this.h / this.inputs.length, 75);
+    this.inputs.forEach((input, idx) => {
+      input.h = availableConnectorHeight - Globals.ConnectorSpacing * 2;
+      input.w = availableConnectorHeight - Globals.ConnectorSpacing * 2;
+      input.y = this.y + idx * availableConnectorHeight + availableConnectorHeight / 2;
+    });
+  }
 
-        this.inputs.forEach((input, idx) => {
-            input.h = availableConnectorHeight - Globals.ConnectorSpacing * 2;
-            input.w = availableConnectorHeight - Globals.ConnectorSpacing * 2;
-            input.y = this.y + idx * availableConnectorHeight + availableConnectorHeight / 2;
-        });
-    }
+  _positionAndScaleOutputs() {
+    const availableConnectorHeight = Math.min(this.h / this.outputs.length, 75);
 
-    _positionAndScaleOutputs() {
-        const availableConnectorHeight = Math.min(this.h / this.outputs.length, 75);
+    this.outputs.forEach((output, idx) => {
+      output.y = this.y + idx * availableConnectorHeight + availableConnectorHeight / 2;
+      output.h = availableConnectorHeight - Globals.ConnectorSpacing * 2;
+      output.w = availableConnectorHeight - Globals.ConnectorSpacing * 2;
+    });
+  }
 
-        this.outputs.forEach((output, idx) => {
-            output.y = this.y + idx * availableConnectorHeight + Globals.ConnectorSpacing;
-            output.h = availableConnectorHeight - Globals.ConnectorSpacing * 2;
-        });
-    }
+  addInput() {
+    const input = new Connector(this.x, 0, 0, this, "input");
+    this.inputs.push(input);
+    this._positionAndScaleInputs();
+    return input;
+  }
 
+  addOutput() {
+    const output = new Connector(this.x + this.w, 0, 0, this, "output");
+    this.outputs.push(output);
+    this._positionAndScaleOutputs();
+    return output;
+  }
 
-    addInput() {
-        const input = new Connector(this.x, 0, 0, this, 'input');
-        this.inputs.push(input);
-        this._positionAndScaleInputs();
-        return input;
-    }
+  removeInput(connector) {
+    const idx = this.inputs.findIndex((c) => c.id === connector.id);
+    if (idx < 0) return;
 
-    addOutput() {
-        const output = new Connector(this.x + this.w, 0, 0, this, 'output');
-        this.outputs.push(output);
-        this._positionAndScaleOutputs();
-        return output;
-    }
+    this.inputs.splice(idx, 1);
+    this._positionAndScaleInputs();
+    state.unregister(connector);
+  }
 
-    removeInput(connector) {
-        const idx = this.inputs.findIndex(c => c.id === connector.id);
-        if (idx < 0) return;
+  render() {
+    this.color.applyFill();
+    this._applyBorder();
 
-        this.inputs.splice(idx, 1);
-        this._positionAndScaleInputs();
-        state.unregister(connector);
-    }
+    rect(this.x, this.y, this.w, this.h);
 
-    render() {
-        this.color.applyFill();
-        this._applyBorder();
+    this.inputs.forEach((input) => input.render());
+    this.outputs.forEach((output) => output.render());
+  }
 
-        rect(this.x, this.y, this.w, this.h);
+  mouseIsOver() {
+    const mX = mouseX;
+    const mY = mouseY;
+    return mX > this.x && mX < this.x + this.w && mY > this.y && mY < this.y + this.h;
+  }
 
-        this.inputs.forEach(input => input.render());
-        this.outputs.forEach(output => output.render());
-    }
-
-    mouseIsOver() {
-        const mX = mouseX;
-        const mY = mouseY;
-        return mX > this.x && mX < this.x + this.w && mY > this.y && mY < this.y + this.h;
-    }
-
-    onClick(button) {
-        console.log(button)
-        if (button === LEFT)
-            state.register(this.addInput());
-        else if (button === RIGHT)
-            state.register(this.addOutput());
-    }
+  onClick(button) {
+    console.log(button);
+    if (button === LEFT) state.register(this.addInput());
+    else if (button === RIGHT) state.register(this.addOutput());
+  }
 }
--- connector.js
+++ connector.js
@@ -1,39 +1,39 @@
 class Connector extends ComponentBase {
-    constructor(x, y, h, parentComponent, type) {
-        super(true, true, false, false);
-        this.x = x;
-        this.y = y;
-        this.color = new Color(20, 360, 60);
-        this.w = Globals.ConnectorWidth;
-        this.h = h;
+  constructor(x, y, h, parentComponent, type) {
+    super(true, true, false, false);
+    this.x = x;
+    this.y = y;
+    this.color = new Color(20, 360, 60);
+    this.w = Globals.ConnectorWidth;
+    this.h = h;
 
-        this.label = "Input " + this.id;
-        this.parentComponent = parentComponent;
-        this.type = type;
-    }
+    this.label = "Input " + this.id;
+    this.parentComponent = parentComponent;
+    this.type = type;
+  }
 
-    _applyBorder() {
-        if (this.mouseIsOver()) {
-            strokeWeight(2);
-            stroke(100, 0, 100);
-        }
-        else {
-            noStroke();
-        }
+  _applyBorder() {
+    if (this.mouseIsOver()) {
+      strokeWeight(2);
+      stroke(100, 0, 100);
+    } else {
+      noStroke();
     }
+  }
 
-    render() {
-        this.color.applyFill();
-        this._applyBorder();
-        arc(this.x, this.y, this.w, this.h, PI / 2, PI / 2 + PI, PIE);
-    }
+  render() {
+    this.color.applyFill();
+    this._applyBorder();
+    if (this.type === "input") arc(this.x, this.y, this.w, this.h, PI / 2, PI / 2 + PI, PIE);
+    else if (this.type === "output") arc(this.x, this.y, this.w, this.h, PI / 2 + PI, PI / 2 + PI + PI, PIE);
+  }
 
-    mouseIsOver() {
-        return dist(mouseX, mouseY, this.x, this.y) < this.w / 2 && mouseX < this.x;
-        //return mX > this.x && mX < this.x + this.w && mY > this.y && mY < this.y + this.h;
-    }
+  mouseIsOver() {
+    if (this.type === "input") return dist(mouseX, mouseY, this.x, this.y) < this.w / 2 && mouseX < this.x;
+    if (this.type === "output") return dist(mouseX, mouseY, this.x, this.y) < this.w / 2 && mouseX > this.x;
+  }
 
-    onClick() {
-        this.parentComponent.removeInput(this);
-    }
+  onClick() {
+    this.parentComponent.removeInput(this);
+  }
 }
