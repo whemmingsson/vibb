355d22b48f2c0fa6b49d1dc613f6ecc47c9c7bc2
William <william.hemmingsson@gmail.com>
2023-04-29 15:32:38 +0200

    Introduced creating wire anchors that will be used for dragging

--- wire.js
+++ wire.js
@@ -11,6 +11,7 @@ class Wire extends ComponentBase {
 
     // Will represent branches of this wire
     this.anchors = [];
+    this.points = [];
   }
 
   _applyStroke() {
@@ -31,23 +32,56 @@ class Wire extends ComponentBase {
     return false;
   }
 
+  _renderAnchorPoint(x, y) {
+
+    strokeWeight(2);
+    ellipse(x, y, 10, 10);
+  }
+
+  _renderLineWithAnchors() {
+    const points = [];
+    points.push({ x: this.from.x, y: this.from.y });
+    points.push(...this.anchors);
+    points.push({ x: this.to.x, y: this.to.y });
+    for (let i = 0; i < points.length - 1; i++) {
+      this._applyStroke();
+      strokeWeight(5);
+      let p1 = points[i];
+      let p2 = points[i + 1];
+      line(p1.x, p1.y, p2.x, p2.y);
+      if (p1.anchor) {
+        ColorScheme.SignalOff.applyFill();
+        ColorScheme.Black.applyStroke();
+        strokeWeight(Globals.strokeWeight);
+        this._renderAnchorPoint(p1.x, p1.y);
+      }
+    }
+  }
+
   render() {
     this._applyStroke();
     strokeWeight(5);
-    line(this.from.x, this.from.y, this.to ? this.to.x : mouseX, this.to ? this.to.y : mouseY);
+    if (this.anchors.length === 0) {
+      line(this.from.x, this.from.y, this.to ? this.to.x : mouseX, this.to ? this.to.y : mouseY);
+    }
+    else {
+      this._renderLineWithAnchors();
+    }
 
     if (this.mouseIsOver()) {
       noFill();
       ColorScheme.White.applyStroke();
-      strokeWeight(2);
-      ellipse(mouseX, mouseY, 10, 10);
+      this._renderAnchorPoint(mouseX, mouseY);
     }
   }
+
+
 
   onClick(button) {
     if (button === RIGHT) {
       this.from.removeWire(this);
       this.to.removeWire(this);
     }
+    this.anchors.push({ x: mouseX, y: mouseY, anchor: true })
   }
 }
